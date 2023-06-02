c0694b4a1a983d0827f27f66b03d931441078e56
Ivan <ivananatolevic66@gmail.com>
2023-06-02 21:20:02 +0300

    add regist react hook form

--- src/App.js
+++ src/App.js
@@ -2,113 +2,62 @@ import { useRef } from 'react';
 import styles from './app.module.css';
 import { useForm } from 'react-hook-form';
 
-
-const sendData = (formData) => {
-	console.log(formData);
-};
-
 function App() {
 	const submitButtonRef = useRef();
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        defaultValues: {
-            email: '',
-            password: '',
-            repeatPassword: '',
-        },
-    });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+		reset,
+		watch,
+	} = useForm({
+		mode: 'onBlur',
+		defaultValues: {
+			email: '',
+			password: '',
+			repeatPassword: '',
+		},
+	});
 
-    const emailProps = {
-        minLength: { value: 3, message: 'Неверный логин. Должно быть не меньше 3 символов' },
-        maxLength: { value: 20, message: 'Неверный логин. Должно быть не больше 20 символов' },
-        pattern: {
-            value: !/^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-            message: 'Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание',
-        },
-    };
-    const passwordProps = {
-        minLength: { value: 3, message: 'Неверный логин. Должно быть не меньше 3 символов' },
-        maxLength: { value: 20, message: 'Неверный логин. Должно быть не больше 20 символов' },
-        pattern: {
-            value: /^[\w_]*$/,
-            message: 'Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание',
-        },
-    };
-    const repeatPasswordProps = {
-        minLength: { value: 3, message: 'Неверный логин. Должно быть не меньше 3 символов' },
-        maxLength: { value: 20, message: 'Неверный логин. Должно быть не больше 20 символов' },
-        pattern: {
-            value: /^[\w_]*$/,
-            message: 'Неверный логин. Допустимые символы: буквы, цифры и нижнее подчёркивание',
-        },
-    };
+	const sendData = (formData) => {
+		console.log(formData);
+		reset();
+	};
+	const password = watch('password', '');
 
+	const emailProps = {
+		required: true,
+		minLength: { value: 5, message: 'Должно быть не меньше 5 символов' },
+		maxLength: { value: 20, message: 'Должно быть не больше 20 символов' },
+		pattern: {
+			value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+			message: 'Некорректный формат email-адреса',
+		},
+	};
+	const passwordProps = {
+		required: true,
+		minLength: { value: 3, message: 'Пароль должно быть не меньше 3 символов' },
+		maxLength: { value: 20, message: 'Пароль должно быть не больше 20 символов' },
+		pattern: {
+			value: /^(?=.*[A-Z])[a-zA-Z0-9]+$/,
+			message:
+				'Пароль должен содержать хотя бы одну заглавную букву и не использовать кирилицу',
+		},
+		validate: (value) => value === password || 'Пароли должны совпадать',
+	};
+	const repeatPasswordProps = {
+		required: true,
+		minLength: { value: 3, message: 'Должно быть не меньше 3 символов' },
+		maxLength: { value: 20, message: 'Должно быть не больше 20 символов' },
+		pattern: {
+			value: /^(?=.*[A-Z])[a-zA-Z0-9]+$/,
+			message:
+				'Пароль должен содержать хотя бы одну заглавную букву и не использовать кирилицу',
+		},
+		validate: (value) => value === password || 'Пароли должны совпадать',
+	};
 
-	// useEffect(() => {
-	// 	if (repeatPassword === '') {
-	// 		setRepeatPasswordError('Поле не может быть пустым');
-	// 	} else if (repeatPassword !== password){
-	// 		setRepeatPasswordError('Пароли не cовпадает');
-	// 	} else {
-	// 		setRepeatPasswordError('');
-	// 	}
-	// }, [repeatPassword, password]);
-
-	// useEffect(() => {
-	// 	if (emailError || passwordError || repeatPasswordError) {
-	// 		setFormValid(false);
-	// 	} else {
-	// 		setFormValid(true);
-	// 		setTimeout(() => {
-	// 			submitButtonRef.current.focus();
-	// 		}, 0);
-	// 	}
-	// }, [emailError, passwordError, repeatPasswordError]);
-
-	// const onEmailChange = ({ target }) => {
-	// 	setEmail(target.value);
-	// 	if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(target.value)) {
-	// 		setEmailErrorr('Некорректный формат email-адреса');
-	// 	} else {
-	// 		setEmailErrorr('');
-	// 	}
-	// };
-
-	// const onPasswordChange = ({ target }) => {
-	// 	setPassword(target.value);
-	// 	if (target.value.length < 3 || target.value.length < 8) {
-	// 		setPasswordError('Пароль должен быть длиной от 3 и не меньше 8 символов');
-	// 	} else if (!/^(?=.*[A-Z])[a-zA-Z0-9]+$/.test(target.value)) {
-	// 		setPasswordError(
-	// 			'Пароль должен содержать хотя бы одну заглавную букву и не использовать кирилицу',
-	// 		);
-	// 	} else {
-	// 		setPasswordError('');
-	// 	}
-	// };
-
-	// const onRepeatPasswordChange = ({ target }) => {
-	// 	setRepeatPassword(target.value);
-	// };
-
-	// const blurHandler = ({ target }) => {
-	// 	if (target.name === 'email') {
-	// 		setEmailBlur(true);
-	// 	} else if (target.name === 'password') {
-	// 		setPasswordBlur(true);
-	// 	} else if (target.name === 'repeat-password') {
-	// 		setRepeatPasswordBlur(true);
-	// 	}
-	// };
-
-	// const onSubmit = (event) => {
-	// 	event.preventDefault();
-	// 	sendData({ email, password });
-	// };
 	const emailError = errors.email?.message;
 	const passwordError = errors.password?.message;
 	const repeatPasswordError = errors.repeatPassword?.message;
@@ -124,11 +73,8 @@ function App() {
 						name="email"
 						placeholder="Почта"
 						{...register('email', emailProps)}
-						// onBlur={blurHandler}
 					/>
-					{emailError && (
-						<div className={styles.errorLabel}>{emailError}</div>
-					)}
+					{emailError && <div className={styles.errorLabel}>{emailError}</div>}
 				</div>
 
 				<div className={styles.form_group}>
@@ -138,11 +84,8 @@ function App() {
 						name="password"
 						placeholder="Пароль"
 						{...register('password', passwordProps)}
-						// onBlur={blurHandler}
 					/>
-					{passwordError && (
-						<div className={styles.errorLabel}>{passwordError}</div>
-					)}
+					{passwordError && <div className={styles.errorLabel}>{passwordError}</div>}
 				</div>
 
 				<div className={styles.form_group}>
@@ -152,16 +95,14 @@ function App() {
 						name="repeat-password"
 						placeholder="Повтор пароля"
 						{...register('repeatPassword', repeatPasswordProps)}
-						// onBlur={blurHandler}
 					/>
 					{repeatPasswordError && (
 						<div className={styles.errorLabel}>{repeatPasswordError}</div>
 					)}
 				</div>
-				<button ref={submitButtonRef} type="submit" >
+				<button ref={submitButtonRef} type="submit" disabled={!isValid}>
 					Зарегистрироваться
 				</button>
-				{/* disabled={!formValid} */}
 			</form>
 		</div>
 	);
